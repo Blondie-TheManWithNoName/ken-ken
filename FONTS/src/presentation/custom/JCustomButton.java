b2f251149248d8c1b7cb8906217a6a8ab1529a09
Oriol Segura Niño <okiko22@gmail.com>
2024-03-11 11:27:14 +0100

    JCustomButton with pointer cursor

--- FONTS/src/presentation/custom/JCustomButton.java
+++ FONTS/src/presentation/custom/JCustomButton.java
@@ -1,10 +1,12 @@
 package presentation.custom;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class JCustomButton extends JButton {
 	public JCustomButton(String text) {
 		super(text);
+		setCursor(new Cursor(Cursor.HAND_CURSOR));
 		setFocusable(false);
 	}
 }
