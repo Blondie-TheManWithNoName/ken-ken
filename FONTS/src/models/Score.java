a61659bc7f70261468c2217ee6ed4b289e9db46d
Oriol Segura Niño <okiko22@gmail.com>
2024-03-14 11:15:18 +0100

    Create Score.java

---
+++ FONTS/src/models/Score.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Score {
+	private final String user;
+	private final int score;
+
+	public Score(String user, int score) {
+		this.user = user;
+		this.score = score;
+	}
+}
