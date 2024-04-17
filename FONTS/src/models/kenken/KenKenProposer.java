c081047487e8616bcf5aa694a451ef30b371b1b6
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-04-17 16:45:22 +0200

    KenKen proposer comments and test

--- FONTS/src/models/kenken/KenKenProposer.java
+++ FONTS/src/models/kenken/KenKenProposer.java
@@ -11,70 +11,149 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 public class KenKenProposer {
+	// Maximum size for a KenKen
 	public static final int MAX_SIZE = 15;
+	// KenKen instance
 	private final KenKen kenKen;
+	// Map containing all the groups with a color
 	private final Map<Group, Color> groupColors = new LinkedHashMap<>();
+	// Map containing all the cells linked to a group
 	private final Map<Cell, Group> cellGroups = new LinkedHashMap<>();
 
+	/**
+	 * Creates a KenKen with the given size
+	 * @param size of the KenKen to be created
+	 */
 	public KenKenProposer(int size) {
 		this.kenKen = new KenKen(size);
 	}
 
+	/**
+	 * Getter for the KenKen size
+	 * @return the size of the KenKen
+	 */
 	public int getSize() {
 		return kenKen.getSize();
 	}
 
+	/**
+	 * Getter for the proposed KenKen
+	 * @returns the KenKen the user is proposing
+	 */
 	public KenKen getKenKen() {
 		return kenKen;
 	}
 
+	/**
+	 * Setter for a fixed cell
+	 * @param row, defines the row of the KenKen to put the value on
+	 * @param col, defines the column of the KenKen to put the value on
+	 * @param value to be set
+	 * @throws ValueOutOfBoundsException when the value is less than 1 or more than the KenKen size
+	 */
 	public void setFixedPosition(int row, int col, int value) throws ValueOutOfBoundsException {
 		kenKen.setFixedPosition(row, col, value);
 	}
 
+	/**
+	 *
+	 * @param row, defines the row of the KenKen to clear the value from
+	 * @param col, defines the column of the KenKen to clear the value from
+	 */
 	public void clearFixedPosition(int row, int col) {
 		kenKen.clearPosition(row, col);
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public boolean anyGroup() {
 		return !groupColors.isEmpty();
 	}
 
+	/**
+	 * Creates a group for the KenKen
+	 * @param operation defines the operation of the group to be created
+	 * @return the created group
+	 */
 	public Group createGroup(Operation operation) {
 		Group group = new Group(operation);
 		groupColors.put(group, ColorFactory.nextColor(new ArrayList<>(groupColors.values())));
 		return group;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public String[] getGroupNotationsEnum() {
 		return IntStream.range(0, groupColors.size()).mapToObj(i -> String.format("%d) %s", i+1, ((Group) groupColors.keySet().toArray()[i]).getNotation())).toArray(String[]::new);
 	}
 
+	/**
+	 *
+	 * @param index
+	 * @return
+	 */
 	public Group getGroup(int index) {
 		return (Group) groupColors.keySet().toArray()[index];
 	}
 
+	/**
+	 * Getter for a group given a cell
+	 * @param row, defines the row of the KenKen to get the group from
+	 * @param col, defines the column of the KenKen to get the group from
+	 * @return the group which the given cell belongs to
+	 */
 	public Group getCellGroup(int row, int col) {
 		return cellGroups.get(kenKen.getCell(row, col));
 	}
 
+	/**
+	 * Getter for the color of a given group
+	 * @param group, defines the group to get the color from
+	 * @return the color of the given group
+	 */
 	public Color getGroupColor(Group group) {
 		return groupColors.get(group);
 	}
 
+	/**
+	 * Adds a cell to a given group
+	 * @param row, defines the row of the KenKen to get the cell from
+	 * @param col, defines the column of the KenKen to get the cell from
+	 * @param group, defines the group to put the cell on
+	 */
 	public void addCellToGroup(int row, int col, Group group) {
 		cellGroups.put(kenKen.getCell(row, col), group);
 	}
 
+	/**
+	 * Removes a given cell from the group it belongs to
+	 * @param row, defines the row of the KenKen to get the cell from
+	 * @param col, defines the column of the KenKen to get the cell from
+	 */
 	public void removeCellGroup(int row, int col) {
 		cellGroups.remove(kenKen.getCell(row, col));
 	}
 
+	/**
+	 * Deletes the given group from the KenKenpropose
+	 * @param group to be deleted
+	 */
 	public void deleteGroup(Group group) {
 		groupColors.remove(group);
 		cellGroups.entrySet().removeIf(entry -> entry.getValue() == group);
 	}
 
+	/**
+	 *
+	 * @throws TooManyOperandsException
+	 * @throws CellAlreadyInGroupException
+	 * @throws CellHasNoGroupException
+	 * @throws GroupCellsNotContiguousException
+	 */
 	public void generateGroups() throws TooManyOperandsException, CellAlreadyInGroupException, CellHasNoGroupException, GroupCellsNotContiguousException {
 		if (cellGroups.size() < getSize() * getSize())
 			throw new CellHasNoGroupException();
---
+++ FONTS/src/tests/KenKenProposerTest.java
@@ -0,0 +1,179 @@
+package tests;
+
+import exceptions.*;
+import models.color.ColorFactory;
+import models.kenken.Group;
+import models.kenken.KenKenProposer;
+import models.operations.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class KenKenProposerTest {
+
+    private KenKenProposer kenKenProposer;
+    private Group[] groups;
+
+    @Before
+    public void init()
+    {
+        kenKenProposer = new KenKenProposer(3);
+        groups = new Group[5];
+        groups[0] = kenKenProposer.createGroup(new OperationSubtraction(2));
+        groups[1] = kenKenProposer.createGroup(new OperationSubtraction(1));
+        groups[2] = kenKenProposer.createGroup(new OperationMultiplication(6));
+        groups[3] = kenKenProposer.createGroup(new OperationDivision(2));
+        groups[4] = kenKenProposer.createGroup(new OperationEquality(1));
+
+        kenKenProposer.addCellToGroup(0, 0, groups[0]);
+        kenKenProposer.addCellToGroup(0, 1, groups[0]);
+
+        kenKenProposer.addCellToGroup(0, 2, groups[1]);
+        kenKenProposer.addCellToGroup(1, 2, groups[1]);
+
+        kenKenProposer.addCellToGroup(1, 0, groups[2]);
+        kenKenProposer.addCellToGroup(2, 0, groups[2]);
+
+        kenKenProposer.addCellToGroup(1, 1, groups[3]);
+        kenKenProposer.addCellToGroup(2, 1, groups[3]);
+
+        kenKenProposer.addCellToGroup(2, 2, groups[4]);
+    }
+
+
+    @Test
+    public void getSizeTest(){
+        assertEquals("Size should be 3", 3, kenKenProposer.getSize());
+    }
+
+    @Test
+    public void setFixedPositionTest() throws  ValueOutOfBoundsException
+    {
+        try {
+            kenKenProposer.setFixedPosition(0, 0, 4);
+            assert false;
+        }
+        catch (ValueOutOfBoundsException e)
+        {
+            assert true;
+        }
+
+        try {
+            kenKenProposer.setFixedPosition(0, 0, 3);
+            assert true;
+        }
+        catch (ValueOutOfBoundsException e)
+        {
+            assert false;
+        }
+    }
+
+    @Test
+    public void getGroupNotationsEnumTest() {
+        String[] notations = {"1) -2", "2) -1", "3) *6", "4) /2", "5) =1"};
+        assertArrayEquals(notations, kenKenProposer.getGroupNotationsEnum());
+    }
+
+    @Test
+    public void getGroupTest() {
+        assertEquals("Group on groups[0] should be getGroup(0)", groups[0], kenKenProposer.getGroup(0));
+        assertEquals("Group on groups[3] should be getGroup(3)", groups[3], kenKenProposer.getGroup(3));
+    }
+
+    @Test
+    public void getCellGroupTest() {
+        assertEquals("Group for cell(0, 0) should be group[0]", kenKenProposer.getCellGroup(0, 0), groups[0]);
+        assertEquals("Group for cell(0, 1) should be group[0]", kenKenProposer.getCellGroup(0, 1), groups[0]);
+
+        assertEquals("Group for cell(0, 2) should be group[1]", kenKenProposer.getCellGroup(0, 2), groups[1]);
+        assertEquals("Group for cell(2, 0) should be group[2]", kenKenProposer.getCellGroup(2, 0), groups[2]);
+        assertEquals("Group for cell(2, 1) should be group[3]", kenKenProposer.getCellGroup(2, 1), groups[3]);
+        assertEquals("Group for cell(2, 2) should be group[4]", kenKenProposer.getCellGroup(2, 2), groups[4]);
+
+        assertNotEquals("Group for cell(2, 2) should not be group[0]", kenKenProposer.getCellGroup(2, 2), groups[0]);
+    }
+
+    @Test
+    public void removeCellGroupTest() {
+        kenKenProposer.removeCellGroup(2, 2);
+        assertNull("Group for cell(2, 2) should be null now", kenKenProposer.getCellGroup(2, 2));
+
+        kenKenProposer.addCellToGroup(2, 2, groups[3]);
+        kenKenProposer.addCellToGroup(2, 2, groups[4]);
+        kenKenProposer.removeCellGroup(2, 2);
+        assertNull("Group for cell(2, 2) should be null now", kenKenProposer.getCellGroup(2, 2));
+
+    }
+
+    @Test
+    public void deleteGroupTest()
+    {
+        kenKenProposer.deleteGroup(groups[0]);
+        assertNotEquals("Group groups[0] should not be now on index 0", groups[0], kenKenProposer.getGroup(0));
+        assertEquals("Group groups[1] should be now on index 0", groups[1], kenKenProposer.getGroup(0));
+        assertNull("Group for cell(0, 0) should be null", kenKenProposer.getCellGroup(0, 0));
+    }
+
+    @Test
+    public void generateGroupsTest() throws  TooManyOperandsException, CellAlreadyInGroupException, CellHasNoGroupException, GroupCellsNotContiguousException
+    {
+        kenKenProposer.removeCellGroup(2, 2);
+
+        try {
+            kenKenProposer.generateGroups();
+            assert false;
+        }
+        catch (CellHasNoGroupException e)
+        {
+            assert true;
+        }
+        kenKenProposer.addCellToGroup(2, 2, groups[3]);
+        try {
+            kenKenProposer.generateGroups();
+            assert false;
+        }
+        catch (TooManyOperandsException e)
+        {
+            assert true;
+        }
+
+        kenKenProposer.addCellToGroup(2, 2, groups[2]);
+
+        try {
+            kenKenProposer.generateGroups();
+            assert false;
+        }
+        catch (GroupCellsNotContiguousException e)
+        {
+            assert true;
+        }
+
+
+        kenKenProposer.addCellToGroup(0, 0, groups[2]);
+
+        try {
+            kenKenProposer.generateGroups();
+            assert false;
+        }
+        catch (CellAlreadyInGroupException e)
+        {
+            assert true;
+        }
+
+        kenKenProposer.removeCellGroup(0, 0);
+        kenKenProposer.addCellToGroup(0, 0, groups[0]);
+        kenKenProposer.removeCellGroup(2, 2);
+        kenKenProposer.addCellToGroup(2, 2, groups[4]); // ERROR
+
+        try {
+            kenKenProposer.generateGroups();
+            assert true;
+        } catch (TooManyOperandsException | GroupCellsNotContiguousException | CellHasNoGroupException |
+                 CellAlreadyInGroupException e) {
+            assert false;
+        }
+
+
+    }
+
+}
