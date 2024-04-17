2b74ccf1c9b79bc40c0a1d009f011bd67f0cc7cf
Oriol Segura Niño <okiko22@gmail.com>
2024-04-17 15:48:10 +0200

    attribute naming

--- FONTS/src/tests/OperationAdditionTest.java
+++ FONTS/src/tests/OperationAdditionTest.java
@@ -6,20 +6,20 @@ import models.operations.OperationAddition;
 import org.junit.Test;
 
 public class OperationAdditionTest {
-    private final static OperationAddition op1 = new OperationAddition(12);
-    private final static OperationAddition op2 = new OperationAddition(22);
-    private final static OperationAddition op3 = new OperationAddition(72);
+    private final static OperationAddition op12 = new OperationAddition(12);
+    private final static OperationAddition op22 = new OperationAddition(22);
+    private final static OperationAddition op72 = new OperationAddition(72);
 
     @Test
     public void check() throws OperandsDoNotMatchException, NonIntegerResultException {
-        assert !op1.check(new int[]{1, 2});
-        assert op1.check(new int[]{1, 3, 8});
+        assert !op12.check(new int[]{1, 2});
+        assert op12.check(new int[]{1, 3, 8});
 
-        assert op2.check(new int[]{9, 9, 4});
-        assert !op2.check(new int[]{1, 5, 9});
+        assert op22.check(new int[]{9, 9, 4});
+        assert !op22.check(new int[]{1, 5, 9});
 
-        assert !op3.check(new int[]{1, 70});
-        assert !op3.check(new int[]{33, 10, 30});
-        assert op3.check(new int[]{10, 30, 20, 12});
+        assert !op72.check(new int[]{1, 70});
+        assert !op72.check(new int[]{33, 10, 30});
+        assert op72.check(new int[]{10, 30, 20, 12});
     }
 }
