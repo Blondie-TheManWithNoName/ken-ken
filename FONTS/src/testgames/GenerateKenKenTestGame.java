7894a7a2fa829c20f9c399f4772990aa4ef5ffc5
Oriol Segura Niño <okiko22@gmail.com>
2024-04-22 21:22:43 +0200

    GenerateKenKenTestGame

--- FONTS/src/testgames/GenerateKenKenTestGame.java
+++ FONTS/src/testgames/GenerateKenKenTestGame.java
@@ -1,9 +1,6 @@
 package testgames;
 
-import exceptions.CellAlreadyInGroupException;
-import exceptions.CellHasNoGroupException;
-import exceptions.GroupCellsNotContiguousException;
-import exceptions.TooManyOperandsException;
+import exceptions.OperandsDoNotMatchException;
 import models.kenken.KenKen;
 import testgames.drivers.*;
 
@@ -11,40 +8,57 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class GenerateKenKenTestGame {
-    private static final KenKenProposerDriver proposer = new KenKenProposerDriver();
     private static final GenerateKenKenDriver generateKenKenDriver = new GenerateKenKenDriver();
+    private static final AiSolveDriver aiSolveDriver = new AiSolveDriver();
+    private static final ExportKenKenDriver exportKenKenDriver = new ExportKenKenDriver();
     private static final PlayKenKenDriver playKenKenDriver = new PlayKenKenDriver();
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        KenKen kenken = null;
+        KenKen kenken;
         int choice;
 
         System.out.println("=== GenerateKenKenTestGame ===\n");
 
-        System.out.println("What would you like to do with the KenKen?");
-        System.out.println("\t1. Generate KenKen");
-        System.out.println("\t2. Play");
-        System.out.println("\t3. Exit\n");
-        do {
-            System.out.print("Choice: ");
-            try {
-                choice = scanner.nextInt();
-            } catch (Exception e) {
-                choice = 0;
-            }
-        } while (choice < 1 || choice > 3);
+		try {
+			kenken = generateKenKenDriver.generate();
+		} catch (OperandsDoNotMatchException e) {
+            System.out.println(e.getMessage());
+            return;
+		}
 
-        switch (choice) {
-            case 1:
-                kenken = generateKenKenDriver.generate();
-                break;
-            case 2:
-                playKenKenDriver.play(kenken);
-                break;
-            case 3:
-                break;
-        }
+		do {
+            System.out.println("What would you like to do with the KenKen?");
+            System.out.println("\t1. AI Solve");
+            System.out.println("\t2. Export");
+            System.out.println("\t3. Play");
+            System.out.println("\t4. Exit\n");
+
+            do {
+                System.out.print("Choice: ");
+                try {
+                    choice = scanner.nextInt();
+                } catch (Exception e) {
+                    choice = 0;
+                }
+            } while (choice < 1 || choice > 4);
+
+            switch (choice) {
+                case 1:
+                    aiSolveDriver.solve(kenken);
+                    break;
+                case 2:
+					try {
+						exportKenKenDriver.export(kenken);
+					} catch (IOException e) {
+                        System.out.println(e.getMessage());
+					}
+					break;
+                case 3:
+                    playKenKenDriver.play(kenken);
+                    break;
+            }
+        } while (choice != 4);
 
         System.out.println("Exiting...");
     }
--- FONTS/src/testgames/drivers/GenerateKenKenDriver.java
+++ FONTS/src/testgames/drivers/GenerateKenKenDriver.java
@@ -6,80 +6,40 @@ import models.kenken.KenKen;
 import models.operations.*;
 import models.topologies.Shape;
 import models.topologies.Topology;
-import models.kenken.KenKenSolver;
-import presentation.views.KenKenSolverView;
-import presentation.views.KenKenView;
 
-import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GenerateKenKenDriver {
-    private static final Shape[] shapes = {};
+    private static final Shape[] shapes = Shape.values();
+    private static final Map<String, Class<? extends Operation>> operations = Map.of(
+            "+", OperationAddition.class,
+            "-", OperationSubtraction.class,
+            "*", OperationMultiplication.class,
+            "/", OperationDivision.class,
+            "gcd", OperationGCD.class,
+            "lcm", OperationLCM.class,
+            "^", OperationPower.class,
+            "=", OperationEquality.class
+    );
+
     private final ModelController controller = new ModelController();
     private final Scanner scanner = new Scanner(System.in);
 
-    private int choice;
-    private int size;
-    private int fixedValues;
-    private Topology topology;
-    private List<Class<? extends Operation>> allowedOperations;
-
-    public KenKen generate() {
-
+    public KenKen generate() throws OperandsDoNotMatchException {
         parametersKenKen();
 
-        do {
-            choice = 0;
-            System.out.println("What would you like to do?");
-            System.out.println("\t1. Generate KenKen");
-            System.out.println("\t2. Change parameters");
-            System.out.println("\t3. View KenKen");
-            System.out.println("\t3. Use KenKen");
-            System.out.println("\t4. Exit\n");
-
-            do {
-                System.out.print("Choice: ");
-                try {
-                    choice = scanner.nextInt();
-                } catch (Exception e) {
-                    choice = 0;
-                }
-            } while (choice < 1 || choice > 4);
-
-            switch (choice) {
-                case 1:
-                    try {
-                        if (controller.generateKenKen(size, fixedValues, topology, allowedOperations)) {
-                            System.out.println("KenKen generated successfully.");
-                        } else {
-                            System.out.println("Failed to generate KenKen.");
-                        }
-                    } catch (OperandsDoNotMatchException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case 2:
-                    parametersKenKen();
-                    break;
-                case 3:
-                    //controller.generatorPlay();
-                    //KenKenView view = new KenKenSolverView(new KenKenSolver(controller.getActiveKenKen()));
-                    //SwingUtilities.invokeLater(view::start);
-                    break;
-                case 4:
-                    return controller.getActiveKenKen();
-                    //break;
-            }
-        } while (choice != 4);
-
-        System.out.println("Exiting KenKen generator...");
-        System.exit(0);
-
-        return null;
+        return controller.getActiveKenKen();
     }
 
-    public void parametersKenKen() {
+    public void parametersKenKen() throws OperandsDoNotMatchException {
+        int size, fixedValues, choice;
+        Topology topology;
+        List<String> operationSymbols = new ArrayList<>(operations.keySet());
+        List<Class<? extends Operation>> allowedOperations = new ArrayList<>();
+
         do {
             System.out.printf("Enter the size of the KenKen (between 2 and %d): ", ModelController.MAX_SIZE);
             try {
@@ -90,77 +50,52 @@ public class GenerateKenKenDriver {
         } while (size < 2 || size > ModelController.MAX_SIZE);
 
         do {
-            System.out.printf("Enter the number of fixed values of the KenKen (between 2 and %d): ", (size * size - 1));
+            System.out.printf("Enter the number of fixed values of the KenKen (between 0 and %d): ", (size * size - 1));
             try {
                 fixedValues = scanner.nextInt();
             } catch (Exception e) {
-                fixedValues = 0;
+                fixedValues = -1;
             }
         } while (fixedValues < 0 || fixedValues > (size * size - 1));
 
+        System.out.print("Possible topologies: ");
+        for (int i = 0; i < shapes.length; i++) {
+            System.out.printf("\n\t%d.", i + 1);
+            for (int[] row : shapes[i].getShape()) {
+                System.out.print("\n\t\t");
+                for (int cell : row)
+                    System.out.print(cell == 1 ? "X" : " ");
+            }
+        }
+        System.out.println("\n");
+
         do {
-            choice = 0;
-            System.out.print("Enter the topology of the KenKen: ");
-            System.out.println("\t1. BLOCK");
-            System.out.println("\t2. J");
-            System.out.println("\t3. L");
-            System.out.println("\t4. T");
-            System.out.println("\t5. I");
-            System.out.println("\t6. DASH");
-            System.out.println("\t7. POINT");
-
-
             System.out.print("Choice: ");
             try {
                 choice = scanner.nextInt();
             } catch (Exception e) {
                 choice = 0;
             }
+        } while (choice < 1 || choice > shapes.length);
 
-            topology = new Topology(Shape, Shape.values[choice]);
+        topology = new Topology(shapes[choice - 1]);
 
-        } while (choice < 1 || choice > 7);
+        System.out.print("Possible KenKen Operations: ");
+        for (int i = 0; i < operationSymbols.size(); i++)
+            System.out.printf("\n\t%d. %s", i + 1, operationSymbols.get(i));
+        System.out.println("\n");
 
         do {
-            choice = 0;
-            System.out.print("Enter the allowed operations of the KenKen: ");
-            System.out.println("\t1. Addition (+)");
-            System.out.println("\t2. Subtraction (-)");
-            System.out.println("\t3. Multiplication (*)");
-            System.out.println("\t4. Division(/)");
-            System.out.println("\t5. Greatest Common Divisor (GCD)");
-            System.out.println("\t6. Power (^)");
-            System.out.println("\t7. Finish");
-
-            do {
-                System.out.print("Choice: ");
-                try {
-                    choice = scanner.nextInt();
-                } catch (Exception e) {
-                    choice = 0;
-                }
-            } while (choice < 1 || choice > 7);
-
-            switch (choice) {
-                case 1:
-                    allowedOperations.add(OperationAddition.class);
-                    break;
-                case 2:
-                    allowedOperations.add(OperationSubtraction.class);
-                    break;
-                case 3:
-                    allowedOperations.add(OperationMultiplication.class);
-                    break;
-                case 4:
-                    allowedOperations.add(OperationDivision.class);
-                    break;
-                case 5:
-                    allowedOperations.add(OperationGCD.class);
-                    break;
-                case 6:
-                    allowedOperations.add(OperationPower.class);
-                    break;
+            System.out.print("Choice (0 to proceed): ");
+            try {
+                choice = scanner.nextInt();
+                if (choice > 0 && choice <= operationSymbols.size())
+                    allowedOperations.add(operations.get(operationSymbols.get(choice - 1)));
+            } catch (Exception e) {
+                choice = -1;
             }
-        } while (choice != 7);
+        } while (choice != 0);
+
+        controller.generateKenKen(size, fixedValues, topology, allowedOperations);
     }
 }
