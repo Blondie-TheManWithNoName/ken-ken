64853da1841b44f4448ed92895c6ea5496d4f191
brian <brian.fernando.arias@estudiantat.fib.upc.edu>
2024-05-15 17:29:00 +0200

    Update MainMenuView.java

--- FONTS/src/presentation/views/MainMenuView.java
+++ FONTS/src/presentation/views/MainMenuView.java
@@ -1,19 +1,20 @@
 package presentation.views;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 import models.kenken.KenKenProposer;
 import presentation.controllers.MainMenuController;
 import presentation.custom.JCustomButton;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class MainMenuView extends JFrame {
+    private final JCustomButton newGameButton = new JCustomButton("New Game");
+    private final JCustomButton loadGameButton = new JCustomButton("Load Game");
 	private final JCustomButton proposeKenKenButton = new JCustomButton("Propose a KenKen");
-	private final JCustomButton generateKenKenButton = new JCustomButton("Generate a KenKen");
-	private final JCustomButton loadKenKenButton = new JCustomButton("Load a KenKen");
-	private final JCustomButton loadSavedGameButton = new JCustomButton("Load a saved game");
-	private final JCustomButton seeRankingButton = new JCustomButton("See ranking");
-	private final JCustomButton exitButton = new JCustomButton("Exit");
+    private final JCustomButton generateKenKenButton = new JCustomButton("Generate a KenKen");
+    private final JCustomButton importKenKenButton = new JCustomButton("Import a KenKen");
+    private final JCustomButton exitButton = new JCustomButton("Exit");
 
 	private final MainMenuController controller = new MainMenuController(this);
 
@@ -49,31 +50,72 @@ public class MainMenuView extends JFrame {
 	private void configureWindow() {
 		setTitle("KenKen");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(false);
+		setSize(400, 300);
+		setResizable(true);
 	}
 
 	private void configureLayout() {
-		setLayout(new GridLayout(7, 1, 0, 0));
+		setLayout(new BorderLayout());
 
+		JPanel backPanel = new JPanel(new GridLayout(3, 3, 2, 2));
+		JPanel mainPanel = new JPanel(new GridLayout(2, 2, 2, 2));
+		JPanel buttonPanel1 = new JPanel(new GridLayout(2, 1, 2, 2));
+		JPanel buttonPanel2 = new JPanel(new GridLayout(3, 1, 2, 2));
+		JLabel game = new JLabel("GAME");
+		JLabel kenKen = new JLabel("KENKEN");
+
+
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		Dimension default1 = new Dimension(120,25);
+		Dimension default2 = new Dimension(120,25);
+
+		proposeKenKenButton.setPreferredSize(default1);
+        proposeKenKenButton.setMinimumSize(default1);
 		proposeKenKenButton.addActionListener(controller);
 		proposeKenKenButton.setActionCommand(MainMenuController.PROPOSE_KENKEN_AC);
+		generateKenKenButton.setPreferredSize(default1);
+        generateKenKenButton.setMinimumSize(default1);
 		generateKenKenButton.addActionListener(controller);
 		generateKenKenButton.setActionCommand(MainMenuController.GENERATE_KENKEN_AC);
-		loadKenKenButton.addActionListener(controller);
-		loadKenKenButton.setActionCommand(MainMenuController.LOAD_KENKEN_AC);
-		loadSavedGameButton.addActionListener(controller);
-		loadSavedGameButton.setActionCommand(MainMenuController.LOAD_SAVED_GAME_AC);
-		seeRankingButton.addActionListener(controller);
-		seeRankingButton.setActionCommand(MainMenuController.SEE_RANKING_AC);
+		importKenKenButton.setPreferredSize(default1);
+        importKenKenButton.setMinimumSize(default1);
+		importKenKenButton.addActionListener(controller);
+		importKenKenButton.setActionCommand(MainMenuController.LOAD_KENKEN_AC);
+		newGameButton.setPreferredSize(default1);
+        newGameButton.setMinimumSize(default1);
+		newGameButton.addActionListener(controller);
+		newGameButton.setActionCommand(MainMenuController.LOAD_SAVED_GAME_AC);
+		loadGameButton.setPreferredSize(default1);
+        loadGameButton.setMinimumSize(default1);
+		loadGameButton.addActionListener(controller);
+		loadGameButton.setActionCommand(MainMenuController.SEE_RANKING_AC);
+		exitButton.setPreferredSize(default1);
+        exitButton.setMinimumSize(default1);
 		exitButton.addActionListener(controller);
 		exitButton.setActionCommand(MainMenuController.EXIT_AC);
 
-		add(proposeKenKenButton);
-		add(generateKenKenButton);
-		add(loadKenKenButton);
-		add(loadSavedGameButton);
-		add(seeRankingButton);
-		add(Box.createVerticalStrut(0));
-		add(exitButton);
+		buttonPanel1.add(newGameButton);
+		buttonPanel1.add(loadGameButton);
+		buttonPanel2.add(proposeKenKenButton);
+		buttonPanel2.add(generateKenKenButton);
+		buttonPanel2.add(importKenKenButton);
+
+	
+		mainPanel.add(buttonPanel1);	
+		mainPanel.add(game);
+		mainPanel.add(kenKen);
+		mainPanel.add(buttonPanel2);
+
+		backPanel.add(exitButton);
+		backPanel.add(new JLabel());
+		backPanel.add(new JLabel());
+		backPanel.add(new JLabel());
+		backPanel.add(mainPanel);
+		backPanel.add(new JLabel());
+		backPanel.add(new JLabel());
+		backPanel.add(new JLabel());
+		backPanel.add(new JLabel());
+
+		add(backPanel, BorderLayout.CENTER);
 	}
 }
