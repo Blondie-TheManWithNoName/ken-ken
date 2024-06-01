6138c76ceb896ddb384e9fb68f07c6dd4cbdce75
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-06-01 19:25:28 +0200

    Sprites GenerateView2

--- FONTS/src/presentation/views/GenerateView2.java
+++ FONTS/src/presentation/views/GenerateView2.java
@@ -7,15 +7,24 @@ import presentation.controllers.Generate2Controller;
 public class GenerateView2 extends MainView {
 	private final PresentationController pController;
 	private final Generate2Controller controller;
-	private final String[] shapeImgs = {"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png",
-										"sprites/select_1.png", "sprites/select_1c.png"};
+	private final String[] shapeImgs = {"sprites/BLOCK_U.png",			"sprites/BLOCK_S.png",
+										"sprites/POINT_U.png",			"sprites/POINT_S.png",
+										"sprites/T_U.png", 				"sprites/T_S.png",
+										"sprites/L_U.png",				"sprites/L_S.png",
+										"sprites/CORNER_U.png", 		"sprites/CORNER_S.png",
+										"sprites/J_U.png",				"sprites/J_S.png",
+										"sprites/I_U.png",				"sprites/I_S.png",
+										"sprites/DASH_U.png",			"sprites/DASH_S.png",
+										"sprites/ZIGZAG_U.png",			"sprites/ZIGZAG_S.png"};
+
+	private final String[] operationImgs = {"sprites/ADDITION_U.png",			"sprites/ADDITION_S.png",
+											"sprites/SUBTRACTION_U.png",		"sprites/SUBTRACTION_S.png",
+											"sprites/MULTIPLICATION_U.png", 	"sprites/MULTIPLICATION_S.png",
+											"sprites/DIVISION_U.png",			"sprites/DIVISION_S.png",
+											"sprites/EQUAL_U.png", 				"sprites/EQUAL_S.png",
+											"sprites/POWER_U.png",				"sprites/POWER_S.png",
+											"sprites/GCD_U.png",				"sprites/GCD_S.png",
+											"sprites/LCM_U.png",				"sprites/LCM_S.png"};
 
 
 	public GenerateView2(PresentationController controller) {
@@ -30,7 +39,6 @@ public class GenerateView2 extends MainView {
 		addSecondRow();
 		addThirdRow();
 		addEmptyRow(5);
-
 		//this.revalidate();
 
 	}
@@ -93,7 +101,7 @@ public class GenerateView2 extends MainView {
 		makeSquare("<html><p style='margin-bottom: -7;'>OPERA</p><p style='margin-top: -7;'>TIONS</p></html>");;
 
 		c.gridx = 2;
-		makeCheckBoxContainer(shapeImgs);
+		makeCheckBoxContainer(operationImgs);
 
 		c.gridx = 3;
 		c.gridheight = 1;
