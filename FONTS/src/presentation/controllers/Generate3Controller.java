a476ac9ad4df508dd2a3ddf36ae35dfb0ec593e3
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-06-02 12:50:06 +0200

    Generate3

--- FONTS/src/presentation/controllers/Generate3Controller.java
+++ FONTS/src/presentation/controllers/Generate3Controller.java
@@ -1,6 +1,10 @@
 package presentation.controllers;
 
-import presentation.views.Generate3View;
+import exceptions.CannotCreateOperationException;
+import exceptions.OperandsDoNotMatchException;
+import exceptions.ShapesAndOperationsDoNotMatchException;
+import presentation.PresentationController;
+import presentation.views.GenerateView3;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,18 +14,33 @@ public class Generate3Controller implements ActionListener {
     public final static String PLAY_AC = "PLAY";
     public final static String BACK_AC = "BACK";
 
-    private final Generate3View view;
+    private final GenerateView3 view;
+    private final PresentationController controller;
 
-    public Generate3Controller(Generate3View view) {this.view = view;}
+    public Generate3Controller(PresentationController controller, GenerateView3 view) {
+        this.controller = controller;
+        this.view = view;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals(REGENERATE_AC)) {
             System.out.println("click regenerate");
+            try {
+                view.generateKenKen();
+            } catch (CannotCreateOperationException ex) {
+                throw new RuntimeException(ex);
+            } catch (OperandsDoNotMatchException ex) {
+                throw new RuntimeException(ex);
+            } catch (ShapesAndOperationsDoNotMatchException ex) {
+                throw new RuntimeException(ex);
+            }
         } else if (e.getActionCommand().equals(PLAY_AC)) {
             System.out.println("click play");
         } else if (e.getActionCommand().equals(BACK_AC)) {
             System.out.println("click back");
+            view.setVisible(false);
+            controller.showGenerateView2();
         }
     }
 }
---
+++ FONTS/src/presentation/views/GenerateView3.java
@@ -0,0 +1,104 @@
+package presentation.views;
+
+import exceptions.CannotCreateOperationException;
+import exceptions.OperandsDoNotMatchException;
+import exceptions.ShapesAndOperationsDoNotMatchException;
+import models.kenken.KenKen;
+import models.operations.Operation;
+import models.topologies.Topology;
+import presentation.PresentationController;
+import presentation.custom.JKenKenCell;
+import presentation.custom.JKenKenPanel;
+import presentation.controllers.*;
+
+import java.util.List;
+
+
+public class GenerateView3 extends MainView {
+
+	private final Generate3Controller controller;
+	private final PresentationController pController;
+	private  JKenKenPanel kenKenPanel;
+	private  KenKen kenKen;
+	private final Topology topology;
+	private final List<Class<? extends Operation>> allowedOperations;
+
+	public GenerateView3(PresentationController controller, List<Class<? extends Operation>> allowedOperations, Topology topology) throws CannotCreateOperationException, OperandsDoNotMatchException, ShapesAndOperationsDoNotMatchException {
+		this.pController = controller;
+		this.topology = topology;
+		this.allowedOperations = allowedOperations;
+		this.controller = new Generate3Controller(pController, this);
+		generateKenKen();
+		start();
+
+		}
+
+	public void generateKenKen() throws CannotCreateOperationException, OperandsDoNotMatchException, ShapesAndOperationsDoNotMatchException {
+		this.kenKen = pController.generateKenKen(allowedOperations, topology);
+		JKenKenCell.CELL_SIZE = 25;
+		this.kenKenPanel = new JKenKenPanel(kenKen);
+		swapKenKen(kenKenPanel);
+		revalidate();
+	}
+
+	public void start() {
+
+		c.gridy = 0;
+		c.gridx = 0;
+		makeSquare("");
+		c.gridwidth = 2;
+		c.gridx = 1;
+		makeSquare("CHECK");
+		c.gridwidth = 1;
+		c.gridx = 3;
+		makeSquare("");
+
+		c.gridy = 1;
+		c.gridx = 0;
+		makeBackButton(controller, Generate3Controller.BACK_AC);
+		c.gridx = 1;
+		c.gridwidth = 2;
+		c.gridheight = 3;
+		makeKenKenPanel(kenKenPanel);
+		c.gridx = 3;
+		c.gridwidth = 1;
+		c.gridheight = 2;
+		makeSquare("");
+
+		c.gridheight = 1;
+		c.gridy = 2;
+		c.gridx = 0;
+		makeSquare("");
+
+
+		c.gridy = 3;
+		c.gridx = 0;
+		makeSquare("");
+		c.gridx = 3;
+		c.gridheight = 2;
+		makeSquare("");
+
+		c.gridheight = 1;
+		c.gridy = 4;
+		c.gridx = 0;
+		makeSquare("");
+		c.gridx = 1;
+		makeButtonSecond("REGENERATE", controller, Generate3Controller.REGENERATE_AC);
+		c.gridx = 2;
+		makeButtonFirst("PLAY", controller, Generate3Controller.PLAY_AC);
+
+		c.gridy = 5;
+		c.gridx = 0;
+		makeSquare("");
+		c.gridx = 1;
+		makeSquare("");
+		c.gridx = 2;
+		makeSquare("");
+		c.gridx = 3;
+		makeSquare("");
+
+		this.revalidate();
+		this.repaint();
+
+	}
+}
