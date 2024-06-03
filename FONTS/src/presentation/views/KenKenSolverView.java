49f3476eb5172d845fbba798d1df9e1eeebb60fd
brian.fernando.arias <brian.fernando.arias@estudiantat.upc.edu>
2024-06-03 07:35:37 +0200

    Update KenKenSolverView.java

--- FONTS/src/presentation/views/KenKenSolverView.java
+++ FONTS/src/presentation/views/KenKenSolverView.java
@@ -7,27 +7,37 @@ import presentation.custom.JCustomButton;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The KenKenSolverView class represents the view for solving KenKen puzzles.
+ */
 public class KenKenSolverView extends KenKenView {
 	private final KenKenSolver solver;
-
-//	private final JCustomButton solveButton = new JCustomButton("Solve");
-
 	private final KenKenSolverController controller = new KenKenSolverController(this);
 
+	/**
+	 * Constructs a KenKenSolverView object.
+	 *
+	 * @param solver The KenKenSolver instance.
+	 */
 	public KenKenSolverView(KenKenSolver solver) {
 		super(solver.getKenKen());
 		this.solver = solver;
 	}
 
+	/**
+	 * Attempts to solve the KenKen puzzle and displays a message if no solution is found.
+	 */
 	public void solve() {
 		if (!solver.solve())
 			JOptionPane.showMessageDialog(this, "No solution found");
 		else {
 			repaint();
-//			solveButton.setEnabled(false);
 		}
 	}
 
+	/**
+	 * Repaints the KenKen puzzle view.
+	 */
 	@Override
 	public void repaint() {
 		super.repaint();
@@ -36,12 +46,11 @@ public class KenKenSolverView extends KenKenView {
 				kenKenPanel.setValue(row, col, kenKen.getValue(row, col), false);
 	}
 
+	/**
+	 * Configures the layout of the KenKenSolverView.
+	 */
 	@Override
 	protected void configureLayout() {
 		super.configureLayout();
-
-//		solveButton.addActionListener(controller);
-//		solveButton.setActionCommand(KenKenSolverController.SOLVE_KENKEN_AC);
-//		add(solveButton, BorderLayout.SOUTH);
 	}
 }
