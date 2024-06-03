725de256a247196f1c7813d6835be5366b0b04b4
brian.fernando.arias <brian.fernando.arias@estudiantat.upc.edu>
2024-06-03 06:47:50 +0200

    Update PauseController.java

--- FONTS/src/presentation/controllers/PauseController.java
+++ FONTS/src/presentation/controllers/PauseController.java
@@ -38,11 +38,11 @@ public class PauseController implements ActionListener {
             }
         } else if (e.getActionCommand().equals(SAVE_AC)) {
 			System.out.println("click save");
-			//path = controller.showSaveDialog();
-			path = controller.showSaveScoreDialog();//solo es para probar ranking
+			path = controller.showSaveDialog();
+			//path = controller.showSaveScoreDialog();//solo es para probar ranking
 			view.setVisible(false);
-			//controller.saveGame(path);
-			controller.saveScore(path);//solo es para probar ranking
+			controller.saveGame(path);
+			//controller.saveScore(path);//solo es para probar ranking
 		} else if (e.getActionCommand().equals(EXPORT_AC)) {
 			System.out.println("click export");
 			path = controller.showExportDialog();
